7ff1a66487d8fcc8a8798cb3e04803469a856723
giosue2007 <toxicgiosue@gmail>
2026-06-18 13:38:10 +0200

    add perm and c

--- app/models/place.py
+++ app/models/place.py
@@ -1,1 +1,9 @@
 @property
+def latitude(self):
+    return self._latitude
+
+@latitude.setter
+def latitude(self, value):
+    if not -90 <= value <= 90:
+        raise ValueError("la latitude doit etre comprise entre -90 et 90")
+    self._latitude = value
